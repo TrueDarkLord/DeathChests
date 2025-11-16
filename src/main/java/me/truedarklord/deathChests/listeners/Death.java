faf8c8096de5219576e2561278a9436af54d05f5
TrueDarkLord <truedarklord.email@gmail.com>
2025-11-16 10:56:21 -0700

    PreDeathChestEvent (#4)
    * PreDeathChestEvent
    
    Add an event that is called each time a player dies.
    
    * Death Message Class
    
    Move the data for the death message over to its own class file, utilising the event generated by the plugin instead of a direct call.
    
    * Better Method
    
    Instead of utilising separate methods to retrieve the item and to remove it, use a single method that does both.

--- src/main/java/me/truedarklord/deathChests/listeners/Death.java
+++ src/main/java/me/truedarklord/deathChests/listeners/Death.java
@@ -1,7 +1,7 @@
 package me.truedarklord.deathChests.listeners;
 
 import me.truedarklord.deathChests.DeathChests;
-import org.bukkit.ChatColor;
+import me.truedarklord.deathChests.events.PreDeathChestEvent;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -13,53 +13,27 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.Plugin;
 
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Death implements Listener {
 
-    private final Plugin plugin;
-
     public Death(DeathChests plugin) {
-        this.plugin = plugin;
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
         Location loc = event.getEntity().getLocation().clone();
         List<ItemStack> drops = event.getDrops();
+
+        PreDeathChestEvent chestEvent = new PreDeathChestEvent(player, loc, drops);
+        if (chestEvent.callEvent()) return;
 
         while (!drops.isEmpty()) {
             setInvDrops(findEmptyBlock(loc), drops);
         }
-
-        sendDeathMessage(event.getEntity(), loc);
-
-    }
-
-    private void sendDeathMessage(Player player, Location loc) {
-        String msg = plugin.getConfig().getString("Death_Message", "&2You Died at &6%world% X:%x% Y:%y% Z:%z%&2.");
-        msg = msg.replaceAll("%world%", loc.getWorld().getName())
-              .replaceAll("%x%", String.valueOf(loc.getBlockX()))
-             .replaceAll("%y%", String.valueOf(loc.getBlockY()))
-             .replaceAll("%z%", String.valueOf(loc.getBlockZ()));
-        
-        player.sendMessage(colour(msg));
-    }
-
-    private static String colour(String message) {
-        Matcher matcher = Pattern.compile("#[a-fA-F\\d]{6}").matcher(message);
-        StringBuilder sb = new StringBuilder();
-
-        while (matcher.find()) {
-            matcher.appendReplacement(sb, net.md_5.bungee.api.ChatColor.of(matcher.group()).toString());
-        }
-
-        return ChatColor.translateAlternateColorCodes('&', matcher.appendTail(sb).toString());
     }
 
     private Block findEmptyBlock(Location loc) {
@@ -84,11 +58,7 @@ public class Death implements Listener {
     }
 
     private ItemStack removeFirst(List<ItemStack> contents) {
-        ItemStack first = contents.get(0);
-
-        contents.remove(0);
-
-        return first;
+        return contents.remove(0);
     }
 
 }
